b8e324520186605284c2465b21cdc8f0b7f580da
Jeff Puckett <jeff@jeffpuckett.com>
2016-11-08 14:41:55 +0000

    add tostring method

--- src/VHostTemplate.php
+++ src/VHostTemplate.php
@@ -5,4 +5,11 @@ class VHostTemplate {
 	public function __construct(String $host){
 		$this->host = $host;
 	}
+
+	public function __toString(){
+		// strip pretty indented tabs seen here, mixed with spaces
+		// http://stackoverflow.com/a/17176793/4233593
+		return preg_replace('/\t+/', '', "
+		");
+	}
 }
