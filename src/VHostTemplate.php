f0ba86f8d9a84718f3bac2c804fda7b58c704359
Jeff Puckett <jeff@jeffpuckett.com>
2016-11-08 14:49:13 +0000

    add document root

--- src/VHostTemplate.php
+++ src/VHostTemplate.php
@@ -1,9 +1,27 @@
 <?php
 namespace jpuck\avhost;
 
+use InvalidArgumentException;
+
 class VHostTemplate {
-	public function __construct(String $host){
+	protected $document_root;
+
+	public function __construct(String $host, String $document_root){
 		$this->host = $host;
+		$this->documentRoot($document_root);
+	}
+
+	public function documentRoot(String $document_root = null) : String {
+		if(isset($document_root)){
+			if(is_dir($document_root)){
+				$this->document_root = $document_root;
+			} else {
+				throw new InvalidArgumentException(
+					"$document_root doesn't exist."
+				);
+			}
+		}
+		return $this->document_root;
 	}
 
 	public function __toString(){
