e5a31ad04805c9fe0d06cf70d5f719f9067343a9
Jeff Puckett <jeff@jeffpuckett.com>
2016-11-08 17:58:11 +0000

    add arguments and options

--- src/Commands/Create.php
+++ src/Commands/Create.php
@@ -12,11 +12,36 @@ class Create extends Command {
 		$this->setName('create')
 			->setDescription('Create a virtual host.')
 			->addArgument(
-				'name',
-				InputArgument::OPTIONAL,
-				'What name would you like to use for the host?'
+				'hostname',
+				InputArgument::REQUIRED,
+				'Name of the virtual host, e.g. www.example.com'
+			)->addArgument(
+				'directory',
+				InputArgument::REQUIRED,
+				'File system folder to serve as document root'
+			)->addOption(
+				'ssl-certificate',
+				'c',
+				InputOption::VALUE_REQUIRED,
+				'SSL certificate'
+			)->addOption(
+				'ssl-key',
+				'k',
+				InputOption::VALUE_REQUIRED,
+				'SSL key'
+			)->addOption(
+				'ssl-chain',
+				'C',
+				InputOption::VALUE_REQUIRED,
+				'SSL certificate intermediate chain'
+			)->addOption(
+				'no-require-ssl',
+				null,
+				InputOption::VALUE_NONE,
+				'Do not redirect plain hosts to encrypted connection'
 			);
 	}
+
 	public function execute(InputInterface $input, OutputInterface $output){
 	}
 }
