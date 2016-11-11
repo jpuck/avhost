42e7a2da7251aec5ecb7321335245ec9b0b47904
Jeff Puckett <jeff@jeffpuckett.com>
2016-11-11 03:27:04 +0000

    test can generate plain vhost conf

--- tests/VHostTemplateTest.php
+++ tests/VHostTemplateTest.php
@@ -2,4 +2,30 @@
 use jpuck\avhost\VHostTemplate;
 
 class VHostTemplateTest extends PHPUnit_Framework_TestCase {
+	protected static $tmp = '/tmp';
+
+	public static function setUpBeforeClass(){
+		$tmp = static::$tmp;
+		if (!is_dir($tmp)) {
+			throw new Exception("$tmp is not a directory.");
+		}
+	}
+
+	public function virtualHostConfigurationDataProvider(){
+		$tmp = static::$tmp;
+		return [
+			'plain' => ['www.example.com', $tmp],
+		];
+	}
+
+	/**
+	 * @dataProvider virtualHostConfigurationDataProvider
+	 */
+	public function testCanGenerateVirtualHostConfiguration($name, $root){
+		$expected = file_get_contents(__DIR__."/confs/$name.conf");
+
+		$actual = (string)(new VHostTemplate($name, $root));
+
+		$this->assertEquals($expected, $actual);
+	}
 }
