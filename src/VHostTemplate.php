d5d0cd0650b8c5fc458d638e7aa8364eb8ecc89c
Jeff Puckett <jeff@jeffpuckett.com>
2016-12-06 17:33:25 +0000

    block access to hidden files and folders

--- src/VHostTemplate.php
+++ src/VHostTemplate.php
@@ -128,6 +128,15 @@ class VHostTemplate {
 		    ServerAdmin webmaster@{$this->hostname}
 		    DocumentRoot {$this->documentRoot}
 
+		    # Block access to all hidden files and directories with the exception of
+		    # the visible content from within the `/.well-known/` hidden directory.
+		    # NOTE: returns 404 resource not found instead of traditional 403 forbidden
+		    RewriteEngine On
+		    RewriteCond %{REQUEST_URI} \"!(^|/)\\.well-known/([^./]+./?)+\$\" [NC]
+		    RewriteCond %{DOCUMENT_ROOT}%{SCRIPT_FILENAME} -d [OR]
+		    RewriteCond %{DOCUMENT_ROOT}%{SCRIPT_FILENAME} -f
+		    RewriteRule \"(^|/)\\.\" - [R=404,L]
+
 		    RewriteEngine On
 		    RewriteCond %{HTTPS} =on
 		    RewriteRule ^ - [env=proto:https]
