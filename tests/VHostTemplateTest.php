5291d09f7e98b06a9b24d3b736c337788ffff601
Jeff Puckett <jeff@jeffpuckett.com>
2017-11-21 20:22:40 +0000

    test can generate ssl conf [ci skip]

--- tests/VHostTemplateTest.php
+++ tests/VHostTemplateTest.php
@@ -18,17 +18,22 @@ class VHostTemplateTest extends PHPUnit_Framework_TestCase
         $tmp = static::$tmp;
         return [
             'plain' => ['example.com', $tmp],
+            'ssl' => ['ssl.example.com', $tmp, [
+                    'key' => '/etc/ssl/private/ssl.example.com.key',
+                    'crt' => '/etc/ssl/certs/ssl.example.com.pem',
+                ],
+            ],
         ];
     }
 
     /**
      * @dataProvider virtualHostConfigurationDataProvider
      */
-    public function testCanGenerateVirtualHostConfiguration($name, $root)
+    public function testCanGenerateVirtualHostConfiguration($name, $root, $options = [])
     {
         $expected = file_get_contents(__DIR__."/confs/$name.conf");
 
-        $actual = (string)(new VHostTemplate($name, $root));
+        $actual = (string)(new VHostTemplate($name, $root, $options));
 
         $this->assertEquals($expected, $actual);
     }
