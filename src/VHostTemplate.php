64771ffd4cc2507782c200f305bbca7fb03cfce8
Jeff Puckett <jeff@jeffpuckett.com>
2017-11-22 00:02:01 +0000

    cs convert tabs to spaces

--- src/VHostTemplate.php
+++ src/VHostTemplate.php
@@ -5,256 +5,256 @@ namespace jpuck\avhost;
 use InvalidArgumentException;
 
 class VHostTemplate {
-	protected $hostname = '';
-	protected $documentRoot = '';
-	protected $ssl = [];
-	protected $options = [
-		'indexes' => false,
-		'realpaths' => true,
-	];
+    protected $hostname = '';
+    protected $documentRoot = '';
+    protected $ssl = [];
+    protected $options = [
+        'indexes' => false,
+        'realpaths' => true,
+    ];
 
-	public function __construct(String $host, String $documentRoot, Array $options = null){
-		$this->hostname($host);
-		$this->documentRoot($documentRoot);
+    public function __construct(String $host, String $documentRoot, Array $options = null){
+        $this->hostname($host);
+        $this->documentRoot($documentRoot);
 
-		if(isset($options)){
-			$this->setOptions($options);
-		}
+        if(isset($options)){
+            $this->setOptions($options);
+        }
 
-		if(isset($options['crt']) || isset($options['key'])){
-			$this->ssl($options);
-		}
-	}
+        if(isset($options['crt']) || isset($options['key'])){
+            $this->ssl($options);
+        }
+    }
 
-	protected function getRealReadableFilename(string $filename, bool $isDirectory = false) : string
-	{
-		if (!$this->options['realpaths']) {
-			return $filename;
-		}
+    protected function getRealReadableFilename(string $filename, bool $isDirectory = false) : string
+    {
+        if (!$this->options['realpaths']) {
+            return $filename;
+        }
 
-		$realpath = realpath($filename);
+        $realpath = realpath($filename);
 
-		if (empty($realpath)) {
-			throw new InvalidArgumentException("$filename is not readable.");
-		}
+        if (empty($realpath)) {
+            throw new InvalidArgumentException("$filename is not readable.");
+        }
 
-		if ($isDirectory && (!is_dir($realpath))) {
-			throw new InvalidArgumentException("$filename is not a directory.");
-		}
+        if ($isDirectory && (!is_dir($realpath))) {
+            throw new InvalidArgumentException("$filename is not a directory.");
+        }
 
-		return $realpath;
-	}
+        return $realpath;
+    }
 
-	protected function setOptions(Array $options){
-		foreach(['indexes', 'forbidden', 'realpaths'] as $option){
-			if(isset($options[$option])){
-				if(!is_bool($options[$option])){
-					throw new InvalidArgumentException(
-						"if declared, $option option must be boolean."
-					);
-				}
-				$this->options[$option] = $options[$option];
-			}
-		}
-	}
+    protected function setOptions(Array $options){
+        foreach(['indexes', 'forbidden', 'realpaths'] as $option){
+            if(isset($options[$option])){
+                if(!is_bool($options[$option])){
+                    throw new InvalidArgumentException(
+                        "if declared, $option option must be boolean."
+                    );
+                }
+                $this->options[$option] = $options[$option];
+            }
+        }
+    }
 
-	public function hostname(String $hostname = null) : String {
-		if(isset($hostname)){
-			if(!ctype_alnum(str_replace(['-','.'], '', $hostname))){
-				throw new InvalidArgumentException(
-					"Hostname may only contain alphanumeric characters."
-				);
-			}
-			$this->hostname = strtolower($hostname);
-		}
-		return $this->hostname;
-	}
+    public function hostname(String $hostname = null) : String {
+        if(isset($hostname)){
+            if(!ctype_alnum(str_replace(['-','.'], '', $hostname))){
+                throw new InvalidArgumentException(
+                    "Hostname may only contain alphanumeric characters."
+                );
+            }
+            $this->hostname = strtolower($hostname);
+        }
+        return $this->hostname;
+    }
 
-	public function documentRoot(String $documentRoot = null) : String {
-		if(isset($documentRoot)){
-			$this->documentRoot = $this->getRealReadableFilename($documentRoot, true);
-		}
+    public function documentRoot(String $documentRoot = null) : String {
+        if(isset($documentRoot)){
+            $this->documentRoot = $this->getRealReadableFilename($documentRoot, true);
+        }
 
-		return $this->documentRoot;
-	}
+        return $this->documentRoot;
+    }
 
-	public function ssl(Array $ssl = null) : Array {
-		if(isset($ssl)){
-			$files = ['crt','key'];
-			if(!empty($ssl['chn'])){
-				$files[]= 'chn';
-			}
+    public function ssl(Array $ssl = null) : Array {
+        if(isset($ssl)){
+            $files = ['crt','key'];
+            if(!empty($ssl['chn'])){
+                $files[]= 'chn';
+            }
 
-			foreach($files as $file){
-				if(!isset($ssl[$file])){
-					throw new InvalidArgumentException(
-						"SSL $file is required."
-					);
-				}
+            foreach($files as $file){
+                if(!isset($ssl[$file])){
+                    throw new InvalidArgumentException(
+                        "SSL $file is required."
+                    );
+                }
 
-				$this->ssl[$file] = $this->getRealReadableFilename($ssl[$file]);
-			}
+                $this->ssl[$file] = $this->getRealReadableFilename($ssl[$file]);
+            }
 
-			// default required
-			$this->ssl['req'] = true;
+            // default required
+            $this->ssl['req'] = true;
 
-			if($this->options['forbidden'] ?? false){
-				$this->ssl['req'] = false;
-			}
+            if($this->options['forbidden'] ?? false){
+                $this->ssl['req'] = false;
+            }
 
-			if(isset($ssl['req'])){
-				if(!is_bool($ssl['req'])){
-					throw new InvalidArgumentException(
-						"if declared, SSL required must be boolean."
-					);
-				}
-				$this->ssl['req'] = $ssl['req'];
-			}
-		}
-		return $this->ssl;
-	}
+            if(isset($ssl['req'])){
+                if(!is_bool($ssl['req'])){
+                    throw new InvalidArgumentException(
+                        "if declared, SSL required must be boolean."
+                    );
+                }
+                $this->ssl['req'] = $ssl['req'];
+            }
+        }
+        return $this->ssl;
+    }
 
-	protected function getDirectoryOptions() : String {
-		if(!empty($this->options['forbidden'])){
-			return "<Directory {$this->documentRoot}>Require all denied</Directory>";
-		}
+    protected function getDirectoryOptions() : String {
+        if(!empty($this->options['forbidden'])){
+            return "<Directory {$this->documentRoot}>Require all denied</Directory>";
+        }
 
-		if($this->options['indexes']){
-			$Indexes = '+Indexes';
-		} else {
-			$Indexes = '-Indexes';
-		}
+        if($this->options['indexes']){
+            $Indexes = '+Indexes';
+        } else {
+            $Indexes = '-Indexes';
+        }
 
-		$options = [
-			"Options $Indexes +FollowSymLinks -MultiViews",
-			'AllowOverride All',
-			'Require all granted',
-		];
+        $options = [
+            "Options $Indexes +FollowSymLinks -MultiViews",
+            'AllowOverride All',
+            'Require all granted',
+        ];
 
-		$optionBlock = PHP_EOL;
-		foreach ($options as $option) {
-			$optionBlock .= $this->indent($option).PHP_EOL;
-		}
+        $optionBlock = PHP_EOL;
+        foreach ($options as $option) {
+            $optionBlock .= $this->indent($option).PHP_EOL;
+        }
 
-		return "<Directory {$this->documentRoot}>$optionBlock</Directory>";
-	}
+        return "<Directory {$this->documentRoot}>$optionBlock</Directory>";
+    }
 
-	protected function configureEssential() : String {
-		$variables = [
-			'hostname' => $this->hostname,
-			'documentRoot' => $this->documentRoot,
-		];
+    protected function configureEssential() : String {
+        $variables = [
+            'hostname' => $this->hostname,
+            'documentRoot' => $this->documentRoot,
+        ];
 
-		return PHP_EOL
-			.$this->getConf('name', $variables).PHP_EOL.PHP_EOL
-			.$this->getConf('blockHidden').PHP_EOL
-			.$this->getConf('redirectToPrimaryHost', $variables).PHP_EOL.PHP_EOL
-			.$this->getDirectoryOptions().PHP_EOL.PHP_EOL
-			.$this->getConf('logging', $variables).PHP_EOL.PHP_EOL
-			.$this->getConf('common')
-			.PHP_EOL;
-	}
+        return PHP_EOL
+            .$this->getConf('name', $variables).PHP_EOL.PHP_EOL
+            .$this->getConf('blockHidden').PHP_EOL
+            .$this->getConf('redirectToPrimaryHost', $variables).PHP_EOL.PHP_EOL
+            .$this->getDirectoryOptions().PHP_EOL.PHP_EOL
+            .$this->getConf('logging', $variables).PHP_EOL.PHP_EOL
+            .$this->getConf('common')
+            .PHP_EOL;
+    }
 
-	protected function getConf(string $name, array $variables = null) : string
-	{
-		$filename = __DIR__."/Templates/$name";
+    protected function getConf(string $name, array $variables = null) : string
+    {
+        $filename = __DIR__."/Templates/$name";
 
-		if (isset($variables)) {
-			extract($variables);
-			return require "$filename.php";
-		}
+        if (isset($variables)) {
+            extract($variables);
+            return require "$filename.php";
+        }
 
-		if (!is_readable("$filename.conf")) {
-			throw new \InvalidArgumentException("$filename.conf is not readable.");
-		}
+        if (!is_readable("$filename.conf")) {
+            throw new \InvalidArgumentException("$filename.conf is not readable.");
+        }
 
-		return file_get_contents("$filename.conf");
-	}
+        return file_get_contents("$filename.conf");
+    }
 
-	protected function configureRequireSSL() : String {
-		if(empty($this->ssl['req'])){
-			return "";
-		}
+    protected function configureRequireSSL() : String {
+        if(empty($this->ssl['req'])){
+            return "";
+        }
 
-		return PHP_EOL.$this->getConf('requireSsl');
-	}
+        return PHP_EOL.$this->getConf('requireSsl');
+    }
 
-	protected function addHstsHeader() : String {
-		if(empty($this->ssl['req'])){
-			return "";
-		}
+    protected function addHstsHeader() : String {
+        if(empty($this->ssl['req'])){
+            return "";
+        }
 
-		return $this->getConf('hsts');
-	}
+        return $this->getConf('hsts');
+    }
 
-	protected function configureHostPlain() : String {
-		return
-			'<VirtualHost *:80>'.PHP_EOL.
-				$this->indent(
-					$this->configureRequireSSL().
-					$this->configureEssential()
-				).
-			'</VirtualHost>'.PHP_EOL;
-	}
+    protected function configureHostPlain() : String {
+        return
+            '<VirtualHost *:80>'.PHP_EOL.
+                $this->indent(
+                    $this->configureRequireSSL().
+                    $this->configureEssential()
+                ).
+            '</VirtualHost>'.PHP_EOL;
+    }
 
-	protected function getSslCertificateLines() : string
-	{
-		if (!isset($this->ssl['crt'])) {
-			return '';
-		}
+    protected function getSslCertificateLines() : string
+    {
+        if (!isset($this->ssl['crt'])) {
+            return '';
+        }
 
-		$sslCertificateLines = [
-			'SSLEngine on',
-			"SSLCertificateFile {$this->ssl['crt']}",
-			"SSLCertificateKeyFile {$this->ssl['key']}",
-		];
+        $sslCertificateLines = [
+            'SSLEngine on',
+            "SSLCertificateFile {$this->ssl['crt']}",
+            "SSLCertificateKeyFile {$this->ssl['key']}",
+        ];
 
-		if(isset($this->ssl['chn'])){
-			$sslCertificateLines []= "SSLCertificateChainFile {$this->ssl['chn']}";
-		}
+        if(isset($this->ssl['chn'])){
+            $sslCertificateLines []= "SSLCertificateChainFile {$this->ssl['chn']}";
+        }
 
-		return implode(PHP_EOL, $sslCertificateLines).PHP_EOL;
-	}
+        return implode(PHP_EOL, $sslCertificateLines).PHP_EOL;
+    }
 
-	protected function configureHostSSL() : String {
-		return
-			'<IfModule mod_ssl.c>'.PHP_EOL.
-		        $this->indent($this->getHostSslContent()).
-			'</IfModule>'.PHP_EOL;
-	}
+    protected function configureHostSSL() : String {
+        return
+            '<IfModule mod_ssl.c>'.PHP_EOL.
+                $this->indent($this->getHostSslContent()).
+            '</IfModule>'.PHP_EOL;
+    }
 
-	protected function getHostSslContent() : string
-	{
-		return
-			'<VirtualHost *:443>'.PHP_EOL.PHP_EOL.
-				$this->indent(
-					$this->addHstsHeader().
-					$this->configureEssential().
-					$this->getSslCertificateLines().PHP_EOL.
-					$this->getConf('sslOptions')
-				).PHP_EOL.
-			'</VirtualHost>'.PHP_EOL;
-	}
+    protected function getHostSslContent() : string
+    {
+        return
+            '<VirtualHost *:443>'.PHP_EOL.PHP_EOL.
+                $this->indent(
+                    $this->addHstsHeader().
+                    $this->configureEssential().
+                    $this->getSslCertificateLines().PHP_EOL.
+                    $this->getConf('sslOptions')
+                ).PHP_EOL.
+            '</VirtualHost>'.PHP_EOL;
+    }
 
-	protected function indent(String $text, Int $length = 1, $indent = "    "){
-		$indentation = $indent;
-		while(--$length){
-			$indentation .= $indent;
-		}
+    protected function indent(String $text, Int $length = 1, $indent = "    "){
+        $indentation = $indent;
+        while(--$length){
+            $indentation .= $indent;
+        }
 
-		$indented = preg_replace('/^/m', $indentation, $text);
+        $indented = preg_replace('/^/m', $indentation, $text);
 
-		// strip those indented newlines
-		return preg_replace('/^    $/m', '', $indented);
-	}
+        // strip those indented newlines
+        return preg_replace('/^    $/m', '', $indented);
+    }
 
-	public function __toString(){
-		$return = $this->configureHostPlain();
+    public function __toString(){
+        $return = $this->configureHostPlain();
 
-		if(!empty($this->ssl)){
-			$return .= PHP_EOL . $this->configureHostSSL();
-		}
+        if(!empty($this->ssl)){
+            $return .= PHP_EOL . $this->configureHostSSL();
+        }
 
-		return $return;
-	}
+        return $return;
+    }
 }
